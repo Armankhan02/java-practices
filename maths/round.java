39a3bcfa92deafc30251dfef3229afaad9ee388a
arman 12102570 <arman.12102570@lpu.in>
2024-08-29 00:56:34 +0530

    methodsQuestion

---
+++ maths/round.java
@@ -0,0 +1,10 @@
+class round{
+    public static void main(String args[]){
+        double num1=3;
+        double num2=49.65;
+        double multi=num1*num2;
+        System.out.println(Math.round(multi));
+
+    }
+    
+}
